62dbd35218361b067698a17e20fd0047ac5a977b
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-07-18 14:59:41 -0400

    moved config file to src and added validation for data

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 const morganOptions = NODE_ENV === 'production' ? 'common' : 'dev';
 app.use(morgan(morganOptions));
+app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
@@ -15,8 +16,30 @@ app.get('/', (req, res) => {
   res.send('A GET Request');
 });
 
-app.post('/', (req, res) => {
+app.post('/register', (req, res) => {
+  //get the data
+  const { username, password, favoriteClub, newsLetter = false } = req.body;
   res.send('POST request received.');
+  //validation code here
+  if (!username) {
+    return res.status(400).send('Username required');
+  }
+  if (!password) {
+    return res.status(400).send('Password required');
+  }
+  if (!favoriteClub) {
+    return res.status(400).send('favorite Club required');
+  }
+  if (username.length < 6 || username.length > 20) {
+    return res.status(400).send('Username must be between 6 and 20 characters');
+  }
+  if(!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)){
+    return res.status(400).send('Password must be contain at least one digit')
+  }
+  //password length
+  if(password.length < 8 || password.length > 36) {
+    return res.status(400).send('Password must be between 8 and 36 characters')
+  }
 });
 
 app.use(function errorHandler(error, req, res, next) {
@@ -25,7 +48,7 @@ app.use(function errorHandler(error, req, res, next) {
     response = { error: { message: 'server error' } };
   } else {
     console.error(error);
-    response = {message: error.message, error};
+    response = { message: error.message, error };
   }
   res.status(500).json(response);
 });
