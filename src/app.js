3283abba96c16ec3ed489c2e8463ba5aa7b94d2a
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-07-18 15:04:05 -0400

    completed validation for data, failing and passing

--- src/app.js
+++ src/app.js
@@ -33,13 +33,29 @@ app.post('/register', (req, res) => {
   if (username.length < 6 || username.length > 20) {
     return res.status(400).send('Username must be between 6 and 20 characters');
   }
-  if(!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)){
-    return res.status(400).send('Password must be contain at least one digit')
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+    return res.status(400).send('Password must be contain at least one digit');
   }
   //password length
-  if(password.length < 8 || password.length > 36) {
-    return res.status(400).send('Password must be between 8 and 36 characters')
+  if (password.length < 8 || password.length > 36) {
+    return res.status(400).send('Password must be between 8 and 36 characters');
   }
+
+  const clubs = [
+    'Cache Valley Stone Society',
+    'Ogden Curling Club',
+    'Park City Curling Club',
+    'Salt City Curling Club',
+    'Utah Olympic Oval Curling Club'
+  ];
+
+  //make sure the club is valid
+  if (!clubs.includes(favoriteClub)) {
+    return res.status(400).send('Not a valid club');
+  }
+
+  //at this point all validation passed
+  res.send('All validation passed');
 });
 
 app.use(function errorHandler(error, req, res, next) {
